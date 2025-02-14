b7f6cac5e544c75ae829a237c6e9077af98caead
Serenayu-qq <yus820@newschool.edu>
2025-02-13 21:01:09 -0500

    1

--- background.js
+++ background.js
@@ -40,3 +40,31 @@ const timer = setInterval(() => {
     }
 
 }, interval);
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const buttons = document.querySelectorAll(".toggle-button");
+
+    buttons.forEach(button => {
+        button.addEventListener("click", () => {
+            const targetMenu = document.getElementById(button.dataset.target);
+
+            // 关闭其他打开的菜单
+            document.querySelectorAll(".dropdown-menu").forEach(menu => {
+                if (menu !== targetMenu) {
+                    menu.style.display = "none";
+                }
+            });
+
+            // 切换当前菜单的显示状态
+            if (targetMenu.style.display === "flex") {
+                targetMenu.style.display = "none";
+            } else {
+                targetMenu.style.display = "flex";
+            }
+        });
+    });
+});
+
+
